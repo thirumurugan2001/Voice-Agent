70f7581c5fb9bbac06a01aa853b4560bbfd45005
thirumurugan2001 <thirusubramaniyan2001@gmail.com>
2026-04-10 19:47:41 +0530

    Add additional CORS origin for localhost:5174

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI
 
 def setup_cors(app: FastAPI):
     origins = [
-        "http://localhost:5173",     
+        "http://localhost:5173", 
+        "http://localhost:5174", 
         "http://127.0.0.1:5173",  
         "http://localhost:3000",    
         "http://127.0.0.1:3000", 

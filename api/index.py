f56d6ab05010da6b7e58016340798d3f8b1fe294
thirumurugan2001 <thirusubramaniyan2001@gmail.com>
2026-04-12 16:14:16 +0530

    Refactor FastAPI setup in index.py and add singlestoredb to requirements.txt

--- api/index.py
+++ api/index.py
@@ -1,3 +1,33 @@
-from main import app
+import sys
+import os
 
-print("🔥 Function loaded successfully")
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import io
+
+from middleware.middleware import setup_cors
+from middleware.controller import voiceAgentController
+from middleware.model import voiceAgent
+
+app = FastAPI()
+setup_cors(app)
+
+@app.post("/chatbot/voice/")
+async def rag(item: voiceAgent):
+    try:
+        response = voiceAgentController(item.base64, item.extension)
+        return StreamingResponse(
+            io.BytesIO(response),
+            media_type="audio/mpeg"
+        )
+    except Exception as e:
+        return {
+            "error": str(e),
+            "statusCode": 500
+        }
+
+@app.get("/")
+def home():
+    return {"message": "OK"}

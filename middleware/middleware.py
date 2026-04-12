7610505fd88180ccef5ef1a5da5a4d5a7305d138
thirumurugan2001 <thirusubramaniyan2001@gmail.com>
2026-04-12 18:38:57 +0530

    Add CORS origin for production frontend in middleware.py

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -7,7 +7,8 @@ def setup_cors(app: FastAPI):
         "http://localhost:5174", 
         "http://127.0.0.1:5173",  
         "http://localhost:3000",    
-        "http://127.0.0.1:3000", 
+        "http://127.0.0.1:3000",    
+        "https://voice-agent-frontend-nu.vercel.app/"
     ]    
     app.add_middleware(
         CORSMiddleware,
